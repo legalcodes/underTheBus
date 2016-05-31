9a8e46984359a1f7e880ad7b2bb04796b872b962
happywhilecoding <jon@expungeus>
2016-05-31 14:15:50 -0700

    Rendered map in index.html file

--- client/UnderTheBus.js
+++ client/UnderTheBus.js
@@ -1,4 +1,64 @@
-var UnderTheBus = angular.module('UnderTheBus', ['ui.router']);
+
+
+var UnderTheBus = angular.module('UnderTheBus', ['ui.router'])
+/*
+		.directive('map', function ($parse) {
+		return {
+				restrict: 'E',
+				replace: true,
+				template: '<div id="map"></div>',
+				link: function (scope, element, attrs) {
+
+
+
+					var width = 900,
+							height = 600;
+
+					var projection = d3.geo.albers()
+								.center([0, 55.4])
+								.rotate([4.4, 0])
+								.parallels([50, 60])
+								.scale(6000)
+								.translate([width / 2, height / 2]);
+
+					var svg = d3.select("body").append("svg")
+								.attr("width", width)
+								.attr("height", height);
+
+					var path = d3.geo.path().projection(projection);
+
+					var g = svg.append("g");
+
+					console.log('getting here');
+
+				d3.json("./sfmaps/world-110m2.json", function(error, topology) {
+						console.log(topology);
+						g.selectAll("path")
+							.data(topojson.feature(topology, topology.objects.countries))
+						.attr('d', d3.geo.path().projection(projection))
+					});
+
+/*
+					d3.json("./sfmaps/arteries.json", function(error, arteries) {
+						if (error) return console.error(error);
+						console.log('arteries: ', arteries);
+
+						svg.append("path")
+							.datum(topojson.feature(arteries, arteries.features))
+							.attr("d", d3.geo.path().projection(d3.geo.mercator()
+																									.scale(500)
+																									.translate([width / 2, height / 2])
+																								 ));
+
+
+
+
+					}); 
+				} 
+			};
+		}); */
+
+
 
 UnderTheBus.config(function($stateProvider, $urlRouterProvider){
 		$urlRouterProvider.otherwise('/viewMap');
@@ -6,7 +66,7 @@ UnderTheBus.config(function($stateProvider, $urlRouterProvider){
 		$stateProvider
 				.state('viewMap', {
 						url: '/viewMap',
-						templateUrl: 'viewMap.html',
+						templateUrl: './viewMap.html',
 						controller: 'mapController'
 				});
 });
@@ -19,4 +79,3 @@ UnderTheBus.controller('mapController', ['$scope', 'MapFactory', function($scope
 UnderTheBus.factory('MapFactory', ['$http', function($http){
 	return {};
 }]);
-
